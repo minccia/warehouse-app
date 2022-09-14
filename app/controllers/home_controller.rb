0dcc3d25c1dc2e27952c44461d3b7bcc4977e8a9
minccia <lusyvasilva@gmail.com>
2022-09-14 13:35:03 -0300

    User sees registered warehouses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index 
-    
+    @warehouses = Warehouse.all 
+    flash.notice = 'Não há galpões cadastrados ainda' if @warehouses.empty?
   end
 end
---
+++ db/migrate/20220913004536_create_warehouses.rb
@@ -0,0 +1,12 @@
+class CreateWarehouses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :warehouses do |t|
+      t.string :name
+      t.string :code
+      t.integer :area
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
