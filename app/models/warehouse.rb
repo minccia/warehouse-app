505bb4c89cefab2cfd5f6d3917e8e152b0cbf919
minccia <lusyvasilva@gmail.com>
2022-09-24 16:34:37 -0300

    Changed method name from corrected_zip_code to formatted_zip_code

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -4,7 +4,7 @@ class Warehouse < ApplicationRecord
   validates :code, length: { is: 3 }
   validates :zip_code, length: { is: 8}
 
-  def corrected_zip_code
+  def formatted_zip_code
     self.zip_code.gsub(/\d{3}$/, '') << '-' << self.zip_code.gsub(/^\d{5}/, '') 
   end
 end
